9092f7ec6da4db599d979b634461b2b93690f9d2
jbourne <sabbiastorm@gmail.com>
2018-10-04 23:17:03 -0600

    working on pdf report

--- scan.py
+++ scan.py
@@ -1,13 +1,23 @@
 import argparse
-from netaddr import IPNetwork
+from netaddr import IPNetwork, IPAddress
 from scapy.all import *
 from tabulate import tabulate
+from fpdf import FPDF
 
 def ip_range(ip_range):
     ip_list = []
-    for ip_addr in IPNetwork(ip_range):
-        ip_list.append(str(ip_addr))
-    return ip_list
+    try:
+        dash_range = re.search('-', ip_range)
+        cidr_range = re.search('/', ip_range)
+        if dash_range is not None or cidr_range is not None:
+            for ip_addr in IPNetwork(ip_range):
+                ip_list.append(str(ip_addr))
+            return ip_list
+        elif IPAddress(ip_range):
+            return str(ip_range)
+    except ValueError:
+        print('Not a valid target IP or range')
+        print_usage()
         
 def TCP_stealth_scan(ip_range, ports):
     active_ports = []
@@ -34,7 +44,7 @@ def ping(ip_range):
     packet = IP(dst=ip_range)/ICMP()
     ans, unans = sr(packet, timeout=2, verbose=False)
     for answer in ans:
-        active_ips.append(answer[1].src)
+        active_ips.append(str(answer[1].src))
     return active_ips
 
 def traceroute(hostnames):
@@ -57,6 +67,7 @@ def traceroute(hostnames):
             data.append([hops, ips])
             hops += 1
     print(tabulate(data, headers=hdrs))
+    return [hdrs, data]
 
 def print_scan_results(results):
     hdrs = ['Host','Active Ports']
@@ -69,32 +80,38 @@ def print_scan_results(results):
         else:
             data.append([ip_addr, ''])
     print(tabulate(data, headers=hdrs))
+    return [hdrs, data]
 
 def print_ping_results(results):
     hdrs = ['Active Hosts']
     data = []
     for ip_addr in results:
         data.append([ip_addr])
-    print(tabulate(results, headers=hdrs))
+    print(tabulate(data, headers=hdrs))
+    return [hdrs, data]
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('target', help='host/ip or ip range to scan in cidr notation')
+    parser.add_argument('target', help='host/ip or ip range to scan in cidr notation or in the form 192.168.207.2-100')
     parser.add_argument('-sT', dest='tcp', action='store_true', help='performs a TCP stealth scan on the target')
     parser.add_argument('-sU', dest='udp', action='store_true', help='performs a UDP scan on the target')
     parser.add_argument('-p', '--port', dest='port', help='port(s) to scan, e.g. 1-100 if a range')
     parser.add_argument('-ps', '--pingsweep', dest='ps', action='store_true', help='performs a ping sweep')
     parser.add_argument('-t', '--traceroute', dest='trace', action='store_true', help='performs a traceroute on the host')
+    parser.add_argument('-r', '--report', dest='pdf', action='store_true', help='prints the results to a pdf report')
     args = parser.parse_args()
     return args
 
 def target_parser(target):
+    # ensures that a target is entered or prints the proper usage
     if target is None:
         print('No target was entered')
         print_usage()
     return target
 
 def ports_parser(ports):
+    # ensures that ports are input in the form 1,600-100 and returns
+    # an integer list of ports
     if ports is None:
         print('Default ports will be used')
         return range(1, 1000)
@@ -119,6 +136,19 @@ def print_usage():
     print('Usage: python3 scapy.py <target> -sT -p <port(s)>')
     sys.exit()
 
+def print_pdf(headers, data):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Courier', 'I', 14)
+    pdf.set_draw_color(255, 255, 255)
+    for header in headers:
+        pdf.cell(len(header)*4, 10, str(header), 1)
+    for elements in data:
+        pdf.ln()
+        for element in elements:
+            pdf.cell(len(str(element))*4, 10, str(element), 1)
+    pdf.output('results.pdf')
+
 if __name__ == '__main__':
     results = {}
     args = create_parser()
@@ -129,16 +159,24 @@ if __name__ == '__main__':
         print('{} hosts are active, starting scan'.format(len(active_ips)))
         for ip_addr in active_ips:
             results[ip_addr] = TCP_stealth_scan(ip_addr, ports)
-        print_scan_results(results)
+        pdf_results = print_scan_results(results)
+        if args.pdf:
+            print_pdf(pdf_results[0], pdf_results[1])
     if args.udp:
         ports = ports_parser(args.port)
         active_ips = ping(target)
         print('{} hosts are active, starting scan'.format(len(active_ips)))
         for ip_addr in active_ips:
             results[ip_addr] = UDP_scan(ip_addr, ports)
-        print_scan_results(results)
+        pdf_results = print_scan_results(results)
+        if args.pdf:
+            print_pdf(pdf_results[0], pdf_results[1])
     if args.ps:
         active_ips = ping(target)
-        print_ping_results(active_ips)
+        pdf_results = print_ping_results(active_ips)
+        if args.pdf:
+            print_pdf(pdf_results[0], pdf_results[1])
     if args.trace:
-        traceroute(target)
+        results = traceroute(target)
+        if args.pdf:
+            print_pdf(pdf_results[0], pdf_results[1])
